453bd2d3807bd2bfda17668c2a0a6881a28a2e22
Filipp Balakin <filipp@balakin.ru>
2022-10-10 16:20:42 +0600

    Revert "copy snapshot stuff from v1"
    This reverts commit e170af4584e372624707b853fe32f9058aff4b1e.

--- dbt/include/clickhouse/macros/materializations/snapshot.sql
+++ dbt/include/clickhouse/macros/materializations/snapshot.sql
@@ -10,214 +10,40 @@
   {{ return(result) }}
 {%- endmacro %}
 
-{% materialization snapshot, adapter='clickhouse' %}
-  {%- set config = model['config'] -%}
 
-  {%- set target_table = model.get('alias', model.get('name')) -%}
+{% macro clickhouse__post_snapshot(staging_relation) %}
+    {{ drop_relation_if_exists(staging_relation) }}
+{% endmacro %}
 
-  {%- set strategy_name = config.get('strategy') -%}
-  {%- set unique_key = config.get('unique_key') %}
+{% macro build_snapshot_staging_table(strategy, sql, target_relation) %}
+    {% set tmp_relation = make_temp_relation(target_relation) %}
 
-  {% if not adapter.check_schema_exists(model.database, model.schema) %}
-    {% do create_schema(model.database, model.schema) %}
-  {% endif %}
+    {% set select = snapshot_staging_table(strategy, sql, target_relation) %}
 
-  {% set target_relation_exists, target_relation = get_or_create_relation(
-          database=none,
-          schema=model.schema,
-          identifier=target_table,
-          type='table') -%}
-
-  {%- if not target_relation.is_table -%}
-    {% do exceptions.relation_wrong_type(target_relation, 'table') %}
-  {%- endif -%}
-
-
-  {{ run_hooks(pre_hooks, inside_transaction=False) }}
-
-  {{ run_hooks(pre_hooks, inside_transaction=True) }}
-
-  {% set strategy_macro = strategy_dispatch(strategy_name) %}
-  {% set strategy = strategy_macro(model, "snapshotted_data", "source_data", config, target_relation_exists) %}
-
-  {% if not target_relation_exists %}
-
-    {% set build_sql = build_snapshot_table(strategy, model['compiled_sql']) %}
-    {% set final_sql = create_table_as(False, target_relation, build_sql) %}
-
-    {% call statement('main') %}
-        {{ final_sql }}
+    {% call statement('build_snapshot_staging_relation') %}
+        {{ create_table_as(False, tmp_relation, select) }}
     {% endcall %}
 
-  {% else %}
+    {% do return(tmp_relation) %}
+{% endmacro %}
 
-    {{ adapter.valid_snapshot_target(target_relation) }}
-
-    {% set staging_table = build_snapshot_staging_table(strategy, sql, target_relation) %}
-
-    {% do adapter.expand_target_column_types(from_relation=staging_table,
-                                             to_relation=target_relation) %}
-
-    {% set missing_columns = adapter.get_missing_columns(staging_table, target_relation)
-                                   | rejectattr('name', 'equalto', 'dbt_change_type')
-                                   | rejectattr('name', 'equalto', 'DBT_CHANGE_TYPE')
-                                   | rejectattr('name', 'equalto', 'dbt_unique_key')
-                                   | rejectattr('name', 'equalto', 'DBT_UNIQUE_KEY')
-                                   | list %}
-
-    {% do create_columns(target_relation, missing_columns) %}
-
-    {% set source_columns = adapter.get_columns_in_relation(staging_table)
-                                 | rejectattr('name', 'equalto', 'dbt_change_type')
-                                 | rejectattr('name', 'equalto', 'DBT_CHANGE_TYPE')
-                                 | rejectattr('name', 'equalto', 'dbt_unique_key')
-                                 | rejectattr('name', 'equalto', 'DBT_UNIQUE_KEY')
-                                 | list %}
-
-    {%- set quoted_source_columns = get_columns_in_query('select * from ' ~ target_relation) -%}
-
-    {% set upsert_relation = target_relation ~ '__snapshot_upsert' %}
-
-    {% do clickhouse__snapshot_merge_sql_one(
-          target = target_relation,
-          source = staging_table,
-          insert_cols = quoted_source_columns,
-          upsert = upsert_relation)
-    %}
-
-    {% call statement('main') %}
-        select 1
-    {% endcall %}
-
-  {% endif %}
-
-  {% do persist_docs(target_relation, model) %}
-
-  {{ run_hooks(post_hooks, inside_transaction=True) }}
-
-  {{ adapter.commit() }}
-
-  {% if staging_table is defined %}
-    {% do post_snapshot(staging_table) %}
-  {% endif %}
-
-  {{ run_hooks(post_hooks, inside_transaction=False) }}
-
-  {{ return({'relations': [target_relation]}) }}
-
-{% endmaterialization %}
-
--- {% macro clickhouse__post_snapshot(staging_relation) %}
---     {{ drop_relation_if_exists(staging_relation) }}
--- {% endmacro %}
-
-{% macro snapshot_staging_table(strategy, source_sql, target_relation) -%}
-  {#- TODO:
-    * add default__snapshot_staging_table to dbt project
-    * replace copy/paste with default__snapshot_staging_table
-  -#}
-  with snapshot_query as (
-      {{ source_sql }}
-  ),
-  snapshotted_data as (
-    select *,
-      {{ strategy.unique_key }} as dbt_unique_key
-    from {{ target_relation }}
-    where dbt_valid_to is null
-  ),
-  insertions_source_data as (
-    select
-      *,
-      {{ strategy.unique_key }} as dbt_unique_key,
-      {{ strategy.updated_at }} as dbt_updated_at,
-      {{ strategy.updated_at }} as dbt_valid_from,
-      nullif({{ strategy.updated_at }}, {{ strategy.updated_at }}) as dbt_valid_to,
-      {{ strategy.scd_id }} as dbt_scd_id
-    from snapshot_query
-  ),
-  updates_source_data as (
-    select
-        *,
-        {{ strategy.unique_key }} as dbt_unique_key,
-        {{ strategy.updated_at }} as dbt_updated_at,
-        {{ strategy.updated_at }} as dbt_valid_from,
-        {{ strategy.updated_at }} as dbt_valid_to
-    from snapshot_query
-  ),
-  {%- if strategy.invalidate_hard_deletes %}
-  deletes_source_data as (
-    select
-      *,
-      {{ strategy.unique_key }} as dbt_unique_key
-    from snapshot_query
-  ),
-  {% endif %}
-  insertions as (
-    select
-      'insert' as dbt_change_type,
-      source_data.*
-    from insertions_source_data as source_data
-    left outer join snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
-    where snapshotted_data.dbt_unique_key is null
-      or (
-      snapshotted_data.dbt_unique_key is not null
-      and (
-        {{ strategy.row_changed }}
-      )
-    )
-  ),
-  updates as (
-    select
-      'update' as dbt_change_type,
-      source_data.*,
-      snapshotted_data.dbt_scd_id
-    from updates_source_data as source_data
-    join snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
-    where (
-      {{ strategy.row_changed }}
-    )
-  )
-  {%- if strategy.invalidate_hard_deletes -%}
-  ,
-  deletes as (
-    select
-      'delete' as dbt_change_type,
-      source_data.*,
-      {{ snapshot_get_time() }} as dbt_valid_from,
-      {{ snapshot_get_time() }} as dbt_updated_at,
-      {{ snapshot_get_time() }} as dbt_valid_to,
-      snapshotted_data.dbt_scd_id
-    from snapshotted_data
-    left join deletes_source_data as source_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
-    where source_data.dbt_unique_key is null
-  )
-  {%- endif %}
-
-  select * from insertions
-  union all
-  select * from updates
-  {%- if strategy.invalidate_hard_deletes %}
-  union all
-  select * from deletes
-  {%- endif %}
-  SETTINGS join_use_nulls=1
-{%- endmacro %}
-
-{% macro clickhouse__snapshot_merge_sql_one(target, source, insert_cols, upsert) -%}
+{% macro clickhouse__snapshot_merge_sql(target, source, insert_cols) -%}
   {%- set insert_cols_csv = insert_cols | join(', ') -%}
+  {%- set valid_to_col = adapter.quote('dbt_valid_to') -%}
 
+  {%- set upsert = target ~ '__snapshot_upsert' -%}
   {% call statement('create_upsert_relation') %}
-    create table if not exists {{ upsert }} as {{ target }};
+    create table if not exists {{ upsert }} as {{ target }}
   {% endcall %}
 
-  {% call statement('insert_unchanged') %}
+  {% call statement('insert_unchanged_data') %}
     insert into {{ upsert }} ({{ insert_cols_csv }})
     select {% for column in insert_cols -%}
       {{ column }} {%- if not loop.last %}, {%- endif %}
     {%- endfor %}
     from {{ target }}
     where dbt_scd_id not in (
-      select {{ source }}.dbt_scd_id from {{ source }}
+      select {{ source }}.dbt_scd_id from {{ source }} 
     )
   {% endcall %}
 
