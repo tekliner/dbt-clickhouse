fab39f205ce9ff44e01bf8448d2e5b209058fa61
Filipp <filipp@balakin.ru>
2022-10-13 15:37:24 +0600

    fix incremental table already exists

--- dbt/include/clickhouse/macros/materializations/incremental.sql
+++ dbt/include/clickhouse/macros/materializations/incremental.sql
@@ -49,6 +49,8 @@
 
     -- First create a temporary table with all of the new data
     {% set new_data_relation = existing_relation.incorporate(path={"identifier": model['name'] + '__dbt_new_data'}) %}
+    {{ drop_relation_if_exists(new_data_relation) }} -- fix for table exists
+
     {% call statement('create_new_data_temp') %}
         {{ get_create_table_as_sql(False, new_data_relation, sql) }}
     {% endcall %}
